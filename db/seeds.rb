e2f60f3933c66780987678903802dd8f2738b948
Deven Clingman <dclingy@gmail.com>
2017-11-09 14:15:37 -0700

    updated seeds file to allow for creation of contacts which belong to users

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,12 @@
 end 
 
 puts "10 users created"
+
+user_ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+names = ["Sam", "Bob", "Gary", "Deven", "Amanda", "Chuck", "Jana", "Derek", "Chad", "Briggs"]
+
+25.times do |c|
+  Contact.create!(name: names.sample, address: "address#{c}", email: "email#{c}@email.com", phone: "1234567892", user_id: user_ids.sample)
+end
+
+puts "25 contacts created"
