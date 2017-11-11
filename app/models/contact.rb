700303f16a5f28528f85b0465d26e4582f167b0d
Deven Clingman <dclingy@gmail.com>
2017-11-11 14:12:12 -0700

    implemented regex for phone and email validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,4 +2,12 @@ class Contact < ApplicationRecord
   belongs_to :user
 
   validates_presence_of :name, :address, :email, :phone
+
+  EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  PHONE_REGEX = /\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]?\d{4}\z/
+
+  validates_format_of :email, with: EMAIL_REGEX 
+
+  validates_format_of :phone, with: PHONE_REGEX
+  validates :phone, length: { is: 10 }
 end
