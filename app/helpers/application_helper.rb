f2202bf311ac475c532f3a1df4f23d57c20c9f15
Deven Clingman <dclingy@gmail.com>
2017-11-11 15:38:10 -0700

    refactored ruby logic for nav bars links into helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,22 @@ module ApplicationHelper
   def alert_generator msg
     js add_gritter(msg, title: "Address Book", sticky: false, time: 2000)
   end
+
+  def nav_links_right
+    if user_signed_in?
+      ("<li>".html_safe + (link_to "Edit Account", edit_user_registration_path) + "</li>".html_safe) 
+      ("<li>".html_safe + (link_to "Logout", destroy_user_session_path, method: :delete) + "</li>".html_safe)
+    else
+      ("<li>".html_safe + (link_to "Login", new_user_session_path) + "</li>".html_safe) +
+      ("<li>".html_safe + (link_to "Register", new_user_registration_path) + "</li>".html_safe)
+    end
+  end
+
+  def nav_links_left
+    if current_user
+      link_to "Welcome #{current_user.first_name}", root_path
+    else
+      "Welcome".html_safe
+    end
+  end
 end
