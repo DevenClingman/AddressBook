83534032d26ae0fbcf1fea1a947c255e86a4bd8d
Deven Clingman <dclingy@gmail.com>
2017-11-13 13:36:52 -0700

    made change to seeds data and changed redirect path of update action to contact index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 
-  User.create!(first_name: "Dee", last_name: "Evans", email: "User@user.com", password: "asdfasdf")
-  User.create!(first_name: "Deven", last_name: "Clingman", email: "User2@user.com", password: "asdfasdf")
+User.create!(first_name: "Dee", last_name: "Evans", email: "User@user.com", password: "asdfasdf")
+User.create!(first_name: "Deven", last_name: "Clingman", email: "User2@user.com", password: "asdfasdf")
 
 
 puts "2 users created"
