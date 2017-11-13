d5611772dcd89ce430ea394284bafc5b04783f21
Deven Clingman <dclingy@gmail.com>
2017-11-13 13:16:26 -0700

    made changes to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,6 @@
-10.times do |u|
-  User.create!(first_name: "FirstName#{u}", last_name: "LastName#{u}", email: "email#{u}@email.com", password: "asdfasdf")
-end 
 
-puts "10 users created"
+  User.create!(first_name: "Dee", last_name: "Evans", email: "User@user.com", password: "asdfasdf")
+  User.create!(first_name: "Deven", last_name: "Clingman", email: "User2@user.com", password: "asdfasdf")
 
-user_ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-names = ["Sam", "Bob", "Gary", "Deven", "Amanda", "Chuck", "Jana", "Derek", "Chad", "Briggs"]
 
-25.times do |c|
-  Contact.create!(name: names.sample, address: "address#{c}", email: "email#{c}@email.com", phone: "1234567892", user_id: user_ids.sample)
-end
-
-puts "25 contacts created"
+puts "2 users created"
